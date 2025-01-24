8bd389fed1dd5e2a88e582db41ea75932e4f0ed5
Leduar07 <leduarhevia@gmail.com>
2025-01-24 23:11:53 +0000

    i

--- .ccls-cache/@home@runner@workspace/Twitter.c
+++ .ccls-cache/@home@runner@workspace/Twitter.c
@@ -3,33 +3,19 @@
 #include <stdlib.h>
 
 int main(void) {
-
-  char text[2555] = "";  
-        
+  char text[3000] = "";  
+  
   //Prints a request for user input & store its value
-  
   printf("Please input text to check:\n");
-  fgets (text, 2555, stdin); 
+  fgets (text, 3000, stdin); 
 
-  printf("\n\n%zu", strlen(text));
-
-
+//Compare the lenght of the input with the predertermined value /account for the null character /if less than 140 print posted, otherwise print rejected*/
   
-  
-  
+  if(strlen(text)>141) {
+    printf("\nRejected.\n");
+  }
+    else {
+    printf("\nPosted.\n");
+  }
   return 0;
-
 }
-
-
-/*
-  if(strlen(text)>140) {
-  printf("\nRejected.\n");
-}
-  if(strlen(text)<=141) {
-  printf("\nPosted.\n");
-}
-// char *textp = (char*) malloc(strlen(text) +1);
-
-/*Compare the lenght of the input with the predertermined value
-if less than 140 print posted, otherwise print rejected*/
